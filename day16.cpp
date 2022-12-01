540b2f280ac3ca806c9ceddbd83577cd7c5259e0
shrutikahilale <shrutikahilale8@gmail.com>
2022-12-01 17:27:56 +0530

    day16

---
+++ day16.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool isPerfect(int n)
+{
+    long sum = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            sum += i;
+        }
+    }
+
+    return sum == n;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    if (isPerfect(n))
+        cout << "Perfect number";
+    else
+        cout << "Not a perfect number";
+
+    return 0;
+}
