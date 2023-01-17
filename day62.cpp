0119413ebd68492274e984579eb39adcac7a2ec3
shrutikahilale <shrutikahilale8@gmail.com>
2023-01-17 17:24:15 +0530

    Gold Mining

---
+++ day62.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, x, y;
+        cin >> n >> x >> y;
+
+        if ((n + 1) * y >= x)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+}
